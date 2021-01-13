5bd43bf4e510d36e73f8ccae1dfb51479d3fc3e2
Marc Cañigueral <m.canigueral@resourcefully.nl>
2021-01-13 18:32:45 +0100

    Remake comments in funciton detect_outliers

--- R/preprocessing.R
+++ R/preprocessing.R
@@ -193,10 +193,10 @@ detect_outliers <- function(sessions, MinPts=NULL, eps=NULL, noise_th = 2, log =
     dbscan_params <- 0
     while (!is.data.frame(dbscan_params)) {
       if (dbscan_params == 1) {
-        # message("Solution not found. Decreasing eps and trying again.")
+        message("Solution not found. Decreasing eps and trying again.")
         eps <- eps/1.5
       } else if (dbscan_params == 2) {
-        # message("Solution not found. Increasing eps and trying again.")
+        message("Solution not found. Increasing eps and trying again.")
         eps <- eps*1.5
       }
       message(paste("Trying with MinPts =", MinPts, "and eps =", eps))
