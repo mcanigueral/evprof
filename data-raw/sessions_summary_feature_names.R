2dd94bace23c6e01519ab851c8bc65e4fd9d82f5
Marc Cañigueral <m.canigueral@resourcefully.nl>
2021-02-05 23:55:16 +0100

    removed data-raw from git

--- data-raw/sessions_summary_feature_names.R
+++
@@ -1,9 +0,0 @@
-sessions_summary_feature_names <- c(
-  "Power",
-  "Energy",
-  "ConnectionHours",
-  "ChargingHours",
-  "FlexibilityHours"
-)
-
-usethis::use_data(sessions_summary_feature_names, overwrite = TRUE)
