ea16bfbf392cd71e1aa721a17bfdf9b3d808799b
Marc Cañigueral <marc.canigueral@udg.edu>
2023-05-16 15:58:40 +0200

    Including california EV sessions when loading the package

--- R/data.R
+++ R/data.R
@@ -23,6 +23,25 @@
 "sessions_summary_feature_names"
 
 
+#' EV charging sessions example
+#'
+#' Example of an charging sessions data set ready to use by `{evprof}` functions.
+#' It is the open source data set downloaded from the
+#' [ACN-Data website](https://ev.caltech.edu/dataset), transformed according
+#' to the standard names defined by `evprof`
+#' (see [this](https://mcanigueral.github.io/evprof/articles/sessions-format.html) article).
+#' More information about the analysis of this data set in the {evprof} website:
+#' <https://mcanigueral.github.io/evprof/articles/california.html>
+#'
+#' @format ## `california_ev_sessions`
+#' A `tibble` object with standard variable names defined by `evprof`
+#' @source <https://ev.caltech.edu/dataset>
+#' @keywords internal
+#'
+"california_ev_sessions"
+
+
+
 #' EV model example
 #'
 #' Example of an `evmodel` object created with `{evprof}` for testing purposes.
